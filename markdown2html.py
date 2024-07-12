e527e9a7c1530ff5d300f590d22ad0331c9ea6a6
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-07-12 07:29:51 -0700

    Start a script

---
+++ markdown2html.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Script markdown2html.py that takes an argument 2 strings
+and converts a Markdown file to HTML
+"""
+import sys
+import os
+import markdown
+
+
+def convert_markdown_to_html(markdown_file, output_file):
+    with open(markdown_file, 'r') as md_file:
+        markdown_text = md_file.read()
+        html_text = markdown.markdown(markdown_text)
+        with open(output_file, 'w') as html_file:
+            html_file.write(html_text)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
+        sys.exit(1)
+
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    if not os.path.isfile(markdown_file):
+        print(f"Missing {markdown_file}", file=sys.stderr)
+        sys.exit(1)
+
+    convert_markdown_to_html(markdown_file, output_file)
+    sys.exit(0)
