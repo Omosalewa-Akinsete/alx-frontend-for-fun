4f91439190bc00815cd8b03868f9e159d74509b3
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-07-14 14:29:54 -0700

    Start a script

--- markdown2html.py
+++ markdown2html.py
@@ -5,26 +5,29 @@ and converts a Markdown file to HTML
 """
 import sys
 import os
-from markdown import markdown
+import markdown
 
 
-if len(sys.argv) < 3:
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    sys.exit(1)
+def convert_markdown_to_html(markdown_file, output_file):
+    with open(markdown_file, 'r') as md_file:
+        markdown_text = md_file.read()
+        html_text = markdown.markdown(markdown_text)
+        with open(output_file, 'w') as html_file:
+            html_file.write(html_text)
 
-markdown_file = sys.argv[1]
-html_file = sys.argv[2]
 
-if not os.path.exists(markdown_file):
-    print(f"Missing {markdown_file}", file=sys.stderr)
-    sys.exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
+        sys.exit(1)
 
-with open(markdown_file, 'r') as f:
-    markdown_content = f.read()
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
 
-html_content = markdown(markdown_content)
+    if not os.path.isfile(markdown_file):
+        print(f"Missing {markdown_file}", file=sys.stderr)
+        sys.exit(1)
 
-with open(html_file, 'w') as f:
-    f.write(html_content)
-
-sys.exit(0)
+    convert_markdown_to_html(markdown_file, output_file)
+    sys.exit(0)
